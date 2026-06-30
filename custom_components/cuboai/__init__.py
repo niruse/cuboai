a6491d32e60f3fa8721fe09171ebf181daf20585
niruse <44327594+niruse@users.noreply.github.com>
2026-06-30 22:20:38 +0300

    Master (#67)
    * style: fix ruff lint errors in async_api.py
    
    * chore: add PyYAML and av to test requirements
    
    * chore: pin aiohttp < 3.11.0 to fix test compatibility
    
    * test: update test assertions for get_camera_profiles
    
    * chore: exclude tutk directory from ruff linting
    
    * fix: add services.yaml and CONFIG_SCHEMA to satisfy hassfest
    
    * test: mock homeassistant.helpers.config_validation
    
    * fix: change import style for config_validation to fix test mocking

--- custom_components/cuboai/__init__.py
+++ custom_components/cuboai/__init__.py
@@ -5,7 +5,7 @@ import aiohttp
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
-import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers import config_validation as cv
 
 from .api.async_api import get_camera_profiles, refresh_cubo_token
 from .api.cuboai_functions import (
